db431bf428fcd23a2c75cfb9336b3c99d481ab85
Enes Yıldırım <enes5519@gmail.com>
2018-02-03 20:44:10 +0100

    MakeServer: Change name of phar

--- src/pocketmine/command/defaults/MakeServerCommand.php
+++ src/pocketmine/command/defaults/MakeServerCommand.php
@@ -48,7 +48,7 @@ class MakeServerCommand extends VanillaCommand{
 
 
         $server = $sender->getServer();
-        $pharPath = Server::getInstance()->getPluginPath() . DIRECTORY_SEPARATOR . "Altay" . DIRECTORY_SEPARATOR . $server->getName() . "_" . $server->getPocketMineVersion() . "_" . date("Y-m-d") . ".phar";
+        $pharPath = Server::getInstance()->getPluginPath() . DIRECTORY_SEPARATOR . "Altay" . DIRECTORY_SEPARATOR . $server->getName() . "_" . $server->getPocketMineVersion() . "_" . $server->getApiVersion() . ".phar";
         if (file_exists($pharPath)) {
             $sender->sendMessage("Phar file already exists, overwriting...");
             @unlink($pharPath);
