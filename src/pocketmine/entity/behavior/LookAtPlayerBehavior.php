ba6c2705c8503f6409db2c9ef284bec7555e99bb
Enes Yıldırım <enes5519@gmail.com>
2018-02-06 20:53:19 +0100

    Fixes and add copyright

--- src/pocketmine/entity/behavior/LookAtPlayerBehavior.php
+++ src/pocketmine/entity/behavior/LookAtPlayerBehavior.php
@@ -1,5 +1,27 @@
 <?php
 
+/*
+ *               _ _
+ *         /\   | | |
+ *        /  \  | | |_ __ _ _   _
+ *       / /\ \ | | __/ _` | | | |
+ *      / ____ \| | || (_| | |_| |
+ *     /_/    \_|_|\__\__,_|\__, |
+ *                           __/ |
+ *                          |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author TuranicTeam
+ * @link https://github.com/TuranicTeam/Altay
+ *
+ */
+
+declare(strict_types=1);
+
 namespace pocketmine\entity\behavior;
 
 use pocketmine\entity\Living;
@@ -19,7 +41,7 @@ class LookAtPlayerBehavior extends Behavior{
 	
 	public function canStart() : bool{
 		if(rand(0,20) === 0){
-			$player = $this->mob->level->getNearestEntity($this, $this->lookDistance, Player::class);
+			$player = $this->mob->level->getNearestEntity($this->mob->asVector3(), $this->lookDistance, Player::class);
 			
 			if($player instanceof Player){
 				$this->player = $player;
