5ba54dad32633ef5151850759fe3af929b065781
Enes Yıldırım <enes5519@gmail.com>
2018-02-13 22:17:40 +0100

    PingCommand: Optimize

--- src/pocketmine/command/defaults/PingCommand.php
+++ src/pocketmine/command/defaults/PingCommand.php
@@ -1,23 +1,46 @@
 <?php
 
+/*
+ *               _ _
+ *         /\   | | |
+ *        /  \  | | |_ __ _ _   _
+ *       / /\ \ | | __/ _` | | | |
+ *      / ____ \| | || (_| | |_| |
+ *     /_/    \_|_|\__\__,_|\__, |
+ *                           __/ |
+ *                          |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author TuranicTeam
+ * @link https://github.com/TuranicTeam/Altay
+ *
+ */
+
+declare(strict_types=1);
+
 namespace pocketmine\command\defaults;
 
 use pocketmine\command\CommandSender;
-use pocketmine\command\overload\CommandParameter;
+use pocketmine\command\overload\CommandParameterUtils;
+use pocketmine\command\utils\InvalidCommandSyntaxException;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 
-class PingCommand extends VanillaCommand {
+class PingCommand extends VanillaCommand{
 
     public function __construct(string $name){
         parent::__construct(
             $name,
-            "%pocketmine.command.ping.description",
-            "%pocketmine.command.ping.usage"
+            "Kendinizin veya başka bir oyuncunun gecikmesini gösterir",
+            "/ping [oyuncu]",
+            [],
+            [CommandParameterUtils::getPlayerParameter()]
         );
-        $this->setPermission("pocketmine.command.ping");
-
-        $this->getOverload("default")->setParameter(0, new CommandParameter("player", CommandParameter::ARG_TYPE_TARGET, true));
+        $this->setPermission("altay.command.ping");
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args){
@@ -25,32 +48,26 @@ class PingCommand extends VanillaCommand {
             return true;
         }
 
+        if(count($args) >= 2){
+            throw new InvalidCommandSyntaxException();
+        }
+
         $target = null;
 
         if(count($args) === 1){
             $target = $sender->getServer()->getPlayer($args[0]);
-        }
-
-        if($target == null){
+        }else{
             if($sender instanceof Player){
                 $target = $sender;
             }else{
-                $sender->sendMessage(TextFormat::RED . "Please provide a player!");
-
-                return true;
+                throw new InvalidCommandSyntaxException();
             }
         }
 
         $ping = $target->getPing();
-        $color = TextFormat::GREEN;
-
-        if($ping >= 150 and $ping <= 250){
-            $color = TextFormat::GOLD;
-        }elseif($ping > 250){
-            $color = TextFormat::RED;
-        }
-
+        $color = ($ping < 150 ? TextFormat::GREEN : ($ping < 250 ? TextFormat::GOLD : TextFormat::RED));
         $sender->sendMessage($target->getName()."'s Ping: ".$color.$ping."ms");
+
         return true;
     }
 }
