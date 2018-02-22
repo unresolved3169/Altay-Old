67411eb20fe46a1a4b3d85dcc673c142d892e810
Enes Yıldırım <enes5519@gmail.com>
2018-02-22 17:19:31 +0100

    BanEntry: work around stupid bug in ext/date
    When plugins do time-limited bans and users enter stupid time values, a shitty bug in ext/date gets triggered, but only when reading the ban entries from disk. DateTime->format() is able to produce formatted strings which have more than 4 digits in the year, which are then considered invalid. This works around it by trying to parse a formatted version on the fly to ensure that it is valid.
    
    This also cleans up and improves ban list loading and handling.
    
    ** from pmmp **

--- src/pocketmine/permission/BanList.php
+++ src/pocketmine/permission/BanList.php
@@ -28,156 +28,162 @@ use pocketmine\utils\MainLogger;
 
 class BanList{
 
-	/** @var BanEntry[] */
-	private $list = [];
+    /** @var BanEntry[] */
+    private $list = [];
 
-	/** @var string */
-	private $file;
+    /** @var string */
+    private $file;
 
-	/** @var bool */
-	private $enabled = true;
+    /** @var bool */
+    private $enabled = true;
 
-	/**
-	 * @param string $file
-	 */
-	public function __construct(string $file){
-		$this->file = $file;
-	}
+    /**
+     * @param string $file
+     */
+    public function __construct(string $file){
+        $this->file = $file;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function isEnabled() : bool{
-		return $this->enabled === true;
-	}
+    /**
+     * @return bool
+     */
+    public function isEnabled() : bool{
+        return $this->enabled === true;
+    }
 
-	/**
-	 * @param bool $flag
-	 */
-	public function setEnabled(bool $flag){
-		$this->enabled = $flag;
-	}
+    /**
+     * @param bool $flag
+     */
+    public function setEnabled(bool $flag){
+        $this->enabled = $flag;
+    }
 
-	/**
-	 * @param string $name
-	 *
-	 * @return BanEntry|null
-	 */
-	public function getEntry(string $name) : ?BanEntry{
-		$this->removeExpired();
+    /**
+     * @param string $name
+     *
+     * @return BanEntry|null
+     */
+    public function getEntry(string $name) : ?BanEntry{
+        $this->removeExpired();
 
-		return $this->list[strtolower($name)] ?? null;
-	}
+        return $this->list[strtolower($name)] ?? null;
+    }
 
-	/**
-	 * @return BanEntry[]
-	 */
-	public function getEntries() : array{
-		$this->removeExpired();
+    /**
+     * @return BanEntry[]
+     */
+    public function getEntries() : array{
+        $this->removeExpired();
 
-		return $this->list;
-	}
+        return $this->list;
+    }
 
-	/**
-	 * @param string $name
-	 *
-	 * @return bool
-	 */
-	public function isBanned(string $name) : bool{
-		$name = strtolower($name);
-		if(!$this->isEnabled()){
-			return false;
-		}else{
-			$this->removeExpired();
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function isBanned(string $name) : bool{
+        $name = strtolower($name);
+        if(!$this->isEnabled()){
+            return false;
+        }else{
+            $this->removeExpired();
 
-			return isset($this->list[$name]);
-		}
-	}
+            return isset($this->list[$name]);
+        }
+    }
 
-	/**
-	 * @param BanEntry $entry
-	 */
-	public function add(BanEntry $entry){
-		$this->list[$entry->getName()] = $entry;
-		$this->save();
-	}
+    /**
+     * @param BanEntry $entry
+     */
+    public function add(BanEntry $entry){
+        $this->list[$entry->getName()] = $entry;
+        $this->save();
+    }
 
-	/**
-	 * @param string    $target
-	 * @param string    $reason
-	 * @param \DateTime $expires
-	 * @param string    $source
-	 *
-	 * @return BanEntry
-	 */
-	public function addBan(string $target, string $reason = null, \DateTime $expires = null, string $source = null) : BanEntry{
-		$entry = new BanEntry($target);
-		$entry->setSource($source ?? $entry->getSource());
-		$entry->setExpires($expires);
-		$entry->setReason($reason ?? $entry->getReason());
+    /**
+     * @param string    $target
+     * @param string    $reason
+     * @param \DateTime $expires
+     * @param string    $source
+     *
+     * @return BanEntry
+     */
+    public function addBan(string $target, string $reason = null, \DateTime $expires = null, string $source = null) : BanEntry{
+        $entry = new BanEntry($target);
+        $entry->setSource($source ?? $entry->getSource());
+        $entry->setExpires($expires);
+        $entry->setReason($reason ?? $entry->getReason());
 
-		$this->list[$entry->getName()] = $entry;
-		$this->save();
+        $this->list[$entry->getName()] = $entry;
+        $this->save();
 
-		return $entry;
-	}
+        return $entry;
+    }
 
-	/**
-	 * @param string $name
-	 */
-	public function remove(string $name){
-		$name = strtolower($name);
-		if(isset($this->list[$name])){
-			unset($this->list[$name]);
-			$this->save();
-		}
-	}
+    /**
+     * @param string $name
+     */
+    public function remove(string $name){
+        $name = strtolower($name);
+        if(isset($this->list[$name])){
+            unset($this->list[$name]);
+            $this->save();
+        }
+    }
 
-	public function removeExpired(){
-		foreach($this->list as $name => $entry){
-			if($entry->hasExpired()){
-				unset($this->list[$name]);
-			}
-		}
-	}
+    public function removeExpired(){
+        foreach($this->list as $name => $entry){
+            if($entry->hasExpired()){
+                unset($this->list[$name]);
+            }
+        }
+    }
 
-	public function load(){
-		$this->list = [];
-		$fp = @fopen($this->file, "r");
-		if(is_resource($fp)){
-			while(($line = fgets($fp)) !== false){
-				if($line{0} !== "#"){
-					$entry = BanEntry::fromString($line);
-					if($entry instanceof BanEntry){
-						$this->list[$entry->getName()] = $entry;
-					}
-				}
-			}
-			fclose($fp);
-		}else{
-			MainLogger::getLogger()->error("Could not load ban list");
-		}
-	}
+    public function load(){
+        $this->list = [];
+        $fp = @fopen($this->file, "r");
+        if(is_resource($fp)){
+            while(($line = fgets($fp)) !== false){
+                if($line{0} !== "#"){
+                    try{
+                        $entry = BanEntry::fromString($line);
+                        if($entry instanceof BanEntry){
+                            $this->list[$entry->getName()] = $entry;
+                        }
+                    }catch(\Throwable $e){
+                        $logger = MainLogger::getLogger();
+                        $logger->critical("Failed to parse ban entry from string \"$line\": " . $e->getMessage());
+                        $logger->logException($e);
+                    }
+                }
+            }
+            fclose($fp);
+        }else{
+            MainLogger::getLogger()->error("Could not load ban list");
+        }
+    }
 
-	/**
-	 * @param bool $flag
-	 */
-	public function save(bool $flag = true){
-		$this->removeExpired();
-		$fp = @fopen($this->file, "w");
-		if(is_resource($fp)){
-			if($flag === true){
-				fwrite($fp, "# Updated " . strftime("%x %H:%M", time()) . " by " . Server::getInstance()->getName() . " " . Server::getInstance()->getPocketMineVersion() . "\n");
-				fwrite($fp, "# victim name | ban date | banned by | banned until | reason\n\n");
-			}
+    /**
+     * @param bool $flag
+     */
+    public function save(bool $flag = true){
+        $this->removeExpired();
+        $fp = @fopen($this->file, "w");
+        if(is_resource($fp)){
+            if($flag === true){
+                fwrite($fp, "# Updated " . strftime("%x %H:%M", time()) . " by " . Server::getInstance()->getName() . " " . Server::getInstance()->getPocketMineVersion() . "\n");
+                fwrite($fp, "# victim name | ban date | banned by | banned until | reason\n\n");
+            }
 
-			foreach($this->list as $entry){
-				fwrite($fp, $entry->getString() . "\n");
-			}
-			fclose($fp);
-		}else{
-			MainLogger::getLogger()->error("Could not save ban list");
-		}
-	}
+            foreach($this->list as $entry){
+                fwrite($fp, $entry->getString() . "\n");
+            }
+            fclose($fp);
+        }else{
+            MainLogger::getLogger()->error("Could not save ban list");
+        }
+    }
 
 }
