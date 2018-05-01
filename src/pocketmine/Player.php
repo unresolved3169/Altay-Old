aa4f2088d97ee1aafe4b5cf75a7548b725888d7d
Enes Yıldırım <enes5519@gmail.com>
2018-05-01 19:04:40 +0200

    Fixed permission on available commands packet

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -701,8 +701,8 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 
 	public function sendCommandData(){
 		$pk = new AvailableCommandsPacket();
-		foreach($this->server->getCommandMap()->getCommands() as $name => $command){
-			if((isset($pk->commandData[$command->getName()]) or $command->getName() === "help") and !$command->testPermissionSilent($this)){
+		foreach($this->server->getCommandMap()->getCommands() as $command){
+			if(!$command->testPermissionSilent($this)){
 				continue;
 			}
 
