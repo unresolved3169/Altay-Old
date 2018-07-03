a9d0d035e73353491ec22330a827e2034fdb8e51
Enes <enes5519@gmail.com>
2018-07-03 18:14:38 +0300

    Fixed duplicate

--- src/pocketmine/command/defaults/MakeServerCommand.php
+++ src/pocketmine/command/defaults/MakeServerCommand.php
@@ -46,7 +46,7 @@ class MakeServerCommand extends VanillaCommand{
         }
 
         $server = $sender->getServer();
-        $pharPath = Server::getInstance()->getPluginPath() . "Altay" . DIRECTORY_SEPARATOR . $server->getName() . "_" . $server->getPocketMineVersion() . "_" . $server->getApiVersion() . ".phar";
+        $pharPath = Server::getInstance()->getPluginPath() . "Altay" . DIRECTORY_SEPARATOR . $server->getName() . "_v" . $server->getApiVersion() . ".phar";
         if (file_exists($pharPath)) {
             $sender->sendMessage("Phar file already exists, overwriting...");
             @unlink($pharPath);
