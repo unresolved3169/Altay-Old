23f608c2f47df92461d8d6e670469cfa4662d647
Enes <enes5519@gmail.com>
2018-07-03 18:18:09 +0300

    Bump

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -38,7 +38,7 @@ namespace pocketmine {
     use pocketmine\wizard\SetupWizard;
 
     const NAME = "Altay";
-    const BASE_VERSION = "4.0.0";
+    const BASE_VERSION = "3.0.5";
     const IS_DEVELOPMENT_BUILD = false;
     const BUILD_NUMBER = 0;
 
